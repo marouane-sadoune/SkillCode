350902c9e02b1dbbc8511e4f8cc5116c788d23b5
marouane-sadoune <marouanesadoune@gmail.com>
2026-01-30 13:02:49 +0100

    setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
+// index.js
+console.log(`Hello ${process.env.HELLO}`)
 
+
+console.log('Database URL:', process.env.PORT);
+dotenv.config();
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Success from backend apinodemon 1212 ee' });
 });
